3e82ceedb4bd2df17b38ebd6048b11b892afc24d
sayabeck <sayabeck@mail.ru>
2022-06-06 21:57:01 +0600

    Initial Commit

---
+++ Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heroes
+//
+//  Created by mac mini on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
